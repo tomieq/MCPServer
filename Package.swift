e9b40c7b67fa5b17880b17afa47adf23489f67ab
tomieq <tomieq@wp.pl>
2026-02-12 10:08:53 +0100

    Inital server setup

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "MCPServer",
+    dependencies: [
+        .package(url: "https://github.com/tomieq/swifter.git", .upToNextMajor(from: "3.1.1"))
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .executableTarget(
+            name: "MCPServer",
+            dependencies: [
+                .product(name: "Swifter", package: "Swifter")
+            ]
+        ),
+    ]
+)
